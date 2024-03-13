e639863aae95b4725a65e0f4568a83268bbd00a8
Gabriela Ortiz Macedo <137196434+GabbyOm@users.noreply.github.com>
2024-03-13 08:21:54 -0600

    Add files via upload

---
+++ Trabajo/src/java/HttpServletResponse.java
@@ -0,0 +1,23 @@
+
+import java.io.PrintWriter;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gaby0
+ */
+class HttpServletResponse {
+
+    void setContentType(String texthtmlcharsetUTF8) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    PrintWriter getWriter() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
